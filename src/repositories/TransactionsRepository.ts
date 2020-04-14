5371fbde3efe17620f3462d9caecf374772b88f4
Ibcs16 <iagobrayhamcunha@hotmail.com>
2020-04-14 16:45:44 -0300

    Create/List transaction and exception when value exceeds total value

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,28 +1,50 @@
 import Transaction from '../models/Transaction';
+import Balance from '../models/Balance';
 
-interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = new Balance();
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    this.updateBalance(transaction);
+    return transaction;
+  }
+
+  private updateBalance({ type, value }: Transaction): Balance {
+    switch (type) {
+      case 'income':
+        this.balance.income += value;
+        break;
+      case 'outcome':
+        this.balance.outcome += value;
+        break;
+      default:
+        break;
+    }
+    this.balance.total = this.balance.income - this.balance.outcome;
+    return this.balance;
   }
 }
 
